533182a0260001aeffa14f800cf1f1acd9101fd1
Vinodkumar Subramaney <vinodgce@gmail.com>
2014-01-28 16:29:06 +0530

    changed router.js

--- router/routes.js
+++ router/routes.js
@@ -10,9 +10,10 @@ module.exports = function (app)
     // router for Problem
     app.get('/problems/random', questionsController.get_problem)
     app.get('/problems/:id', questionsController.get_problem_by_id)
-    //app.get('/problems', questionsController.get_problems)
-    app.get('/problems/:n', questionsController.get_n_problems)
+    app.get('/problems/poll/:n', questionsController.get_n_problems)
     app.post('/problems', questionsController.createProblem)
+
+    //app.get('/problems', questionsController.get_problems)
 
     // router for User
     app.get('/user/:id', userController.getUserById)
